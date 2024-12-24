432bc65b220c4ae6f3d0854b26291ce5635d0ab3
Evair Domingues <evairbd@gmail.com>
2024-12-23 21:50:55 -0300

    feat: models recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(models.Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(models.Category, CategoryAdmin)
--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from utils.recipes.factory import  make_recipe
 
 
 def home(request):
-    context = {'nome':'Evair'}
+    context = {'recipes':[make_recipe() for _ in range(10)]}
     return render(request, 'recipes/pages/home.html', context=context)
 
 def recipe(request, id):
-    context = {'nome':'Evair'}
+    context = {
+        'recipe':make_recipe(),
+        'is_detail_page': True,
+    }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
