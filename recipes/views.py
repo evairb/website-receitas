bec1b79ec37d0e39def3b1c1fc1374411059e837
Evair Domingues <evairbd@gmail.com>
2025-01-03 15:01:50 -0300

    feat: load recipes models

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import  make_recipe
-
+from recipes.models import Recipe
 
 def home(request):
-    context = {'recipes':[make_recipe() for _ in range(10)]}
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    context = {
+        'recipes':recipes
+        }
     return render(request, 'recipes/pages/home.html', context=context)
 
-def recipe(request, id):
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+                                Recipe.objects.filter(
+                                    category__id=category_id, 
+                                    is_published=True).order_by('-id')
+                            )
     context = {
-        'recipe':make_recipe(),
+        'recipes':recipes,
+        'title': f'{recipes[0].category.name} - Category |'
+        }
+    return render(request, 'recipes/pages/category.html', context=context)
+
+
+
+
+def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id,
+        is_published=True
+    ).first()
+    context = {
+        'recipe':recipe,
         'is_detail_page': True,
     }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
