eb7c960ef7eead13fc76964257430f39085646fb
Evair Domingues <evairbd@gmail.com>
2025-01-17 09:38:41 -0300

    feat: test recipe

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,16 @@
+from django.test import TestCase  # type: ignore
+from django.urls import reverse  # type: ignore
+
+
+class RecipeURLsTest(TestCase):
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', args=(1, ))
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, get_list_or_404
-from utils.recipes.factory import  make_recipe
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from recipes.models import Recipe
+
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     context = {
-        'recipes':recipes
+        'recipes': recipes
         }
     return render(request, 'recipes/pages/home.html', context=context)
 
@@ -13,25 +13,25 @@ def home(request):
 def category(request, category_id):
     recipes = get_list_or_404(
                                 Recipe.objects.filter(
-                                    category__id=category_id, 
+                                    category__id=category_id,
                                     is_published=True).order_by('-id')
                             )
     context = {
-        'recipes':recipes,
+        'recipes': recipes,
         'title': f'{recipes[0].category.name} - Category |'
         }
     return render(request, 'recipes/pages/category.html', context=context)
 
 
-
-
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
+    recipe = get_object_or_404(
+        Recipe,
         id=id,
         is_published=True
-    ).first()
+    )
+
     context = {
-        'recipe':recipe,
+        'recipe': recipe,
         'is_detail_page': True,
     }
     return render(request, 'recipes/pages/recipe-view.html', context=context)
